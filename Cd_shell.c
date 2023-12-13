c582d54cddddad25bb175fe50e9714218d3f85be
“RoseleneOdimgbe” <babymesa98@gmail.com>
2023-12-13 09:53:45 -0800

    Cd_shell.c

--- Cd_shell.c
+++ Cd_shell.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * cd_shell - changes current directory
- *
- * @datash: data relevant
+ * cd_shell - this changes current directory
+ * @datash: the relevant data
  * Return: 1 on success
  */
+
 int cd_shell(data_shell *datash)
 {
 	char *dir;
