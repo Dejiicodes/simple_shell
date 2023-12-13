9ded486d8eaa76cea88c396645e095b4f567a190
“RoseleneOdimgbe” <babymesa98@gmail.com>
2023-12-13 06:20:23 -0800

    Exec_line.c

---
+++ Exec_line.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * exec_line - this finds built-ins and commands
+ * @datash: the relevant data (args)
+ * Return: 1 if successful
+ */
+
+int exec_line(data_shell *datash)
+{
+	int (*builtin)(data_shell *datash);
+
+	if (datash->args[0] == NULL)
+		return (1);
+
+	builtin = get_builtin(datash->args[0]);
+
+	if (builtin != NULL)
+		return (builtin(datash));
+
+	return (cmd_exec(datash));
+}
